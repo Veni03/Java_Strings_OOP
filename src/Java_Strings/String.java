c37a5b557aca869167d7eba27d585742d89066de
Venislav <venba@abv.bg>
2020-07-15 10:05:20 +0300

    String using program witch print first and last letter and the length of the string

---
+++ src/Java_Strings/String.java
@@ -0,0 +1,13 @@
+package Java_Strings;
+import java.util.Scanner;
+
+public class String {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        java.lang.String myName = input.nextLine();
+        System.out.println("First Letter is: " + myName.charAt(0));
+        System.out.println("Last Letter is: " + myName.charAt(myName.length() - 1));
+        System.out.println("Length: " + myName.length());
+    }
+
+}
