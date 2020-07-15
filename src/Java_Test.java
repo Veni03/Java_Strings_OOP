2b5ac0d1b1e19bb7343ad2ed8e511027bf8cd538
Venislav <venba@abv.bg>
2020-07-15 14:51:13 +0300

    Java_Test

---
+++ src/Java_Test.java
@@ -0,0 +1,52 @@
+public class Java_Test {
+    public static void main(String[] args) {
+        System.out.println("JAVA TEST 3");
+        System.out.println();
+        System.out.println("Задача 1:");
+        System.out.println("Int - пази цели числа");
+        System.out.println("Double - пази дестични числа");
+        System.out.println("float - пази десетични числа с по-малък обем");
+        System.out.println("long - пази най-голям диапазон от цели числа");
+        System.out.println("byte - пази числа само от -128 до 127");
+        System.out.println("enum");
+        System.out.println("String - пази текс");
+        System.out.println("Char - пази един символ");
+        System.out.println();
+        System.out.println();
+        System.out.println("Задача 2:");
+        System.out.println("Масив е сбор от числа събрани под една променлива");
+        System.out.println("int a[] = new int[20] *20 елемента в масива");
+        System.out.println("int a[] = {5,4,3,6,3,6,4} *елементите на масива");
+        System.out.println();
+        System.out.println();
+        System.out.println("Задача 3:");
+        System.out.println("for (int i = 0; i < matrix.length; i++) {\n" +
+                " for (int j = 0; j < matrix[0].length; j++) {\n" +
+                " System.out.print(matrix[i][j]);\n" +
+                " }\n" +
+                " System.out.println();");
+        System.out.println();
+        System.out.println();
+        System.out.println("Задача 4:");
+        System.out.println("IF Else се използва при проверка на условие");
+        System.out.println("if(a>b){\n" + "System.out.println(a)\n" + "}else{\n" + "System.out.println(b)\n" + "}");
+        System.out.println();
+        System.out.println();
+        System.out.println("Задача 5:");
+        System.out.println("For е цикъл и се използва за обхождане");
+        System.out.println("for (int i = 0; i <10 ; i++) {\n" + "System.out.println(i) }*извежда числата от 0 до 9");
+        System.out.println();
+        System.out.println();
+        System.out.println("Задача 6:");
+        System.out.println("break е използва за стопиране при проверка тип switch, прекратява проверката по-нататък ако условието е вярно");
+        int n = 1;
+        for (int i = 1; i <= 100; i++) {
+            n *= i;
+            System.out.println();
+            System.out.println(n);
+
+        }
+
+
+    }
+}
